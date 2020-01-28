28a6ae84c6964d9e8f082c1094a747b8d1d8c736
Quemio <quemio@gmail.com>
2020-01-28 20:53:19 +0100

    gisteren voordat ik naar huis ging

--- Script/script.js
+++ Script/script.js
@@ -2,10 +2,10 @@
 
 function verbergFilter() {
 	var x = document.getElementById("hideandshow");
-	if (x.style.display === "none") {
-	x.style.display = "block";
-	} else {
+	if (x.style.display === "block") {
 	x.style.display = "none";
+	} else {
+	x.style.display = "block";
 	}
 }
 
