7538dc9a4c505528c3d95d6e41c843378e8ea0a6
Quemio <quemio@gmail.com>
2020-01-29 12:51:30 +0100

    Final aanpassingen

--- Script/script.js
+++ Script/script.js
@@ -2,10 +2,10 @@
 
 function verbergFilter() {
 	var x = document.getElementById("hideandshow");
-	if (x.style.display === "block") {
-	x.style.display = "none";
-	} else {
+	if (x.style.display === "none") {
 	x.style.display = "block";
+	} else {
+	x.style.display = "none";
 	}
 }
 
@@ -13,11 +13,13 @@ function verbergFilter() {
 
 // openen van zoekknop
 
-//declaratie van dom elementen
+// declaratie van dom elementen
 var zoekveld = document.querySelector('body>header');
 var zoekbutton = document.querySelector('body>header>button');
 
 //actie !
+
+
 zoekbutton.addEventListener("click",function(){
 	zoekveld.classList.toggle('show-search');
 });
